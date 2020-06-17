5891844de42045eb1c21300b0de2ec64d61719b2
Rishat Sharafiev <rishatsharafiev@ya.ru>
2020-06-17 14:27:57 +0300

    Save results 4

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 1. создать токен по картинке create_token.png
 2. скачать файл credentials.json
 3. задать переменные в .env
+
+credentials.json - хранится начальные токены
+credentials_refresh.json - храняться промежуточные токены
--- pydrive_upload.py
+++ pydrive_upload.py
@@ -1,12 +1,30 @@
+import os
 from decouple import config
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 
 gauth = GoogleAuth()
 gauth.settings.update({
+    'client_config_backend': 'file',
     'client_config_file': 'credentials.json',
+    'save_credentials': False,
+    'oauth_scope': ['https://www.googleapis.com/auth/drive']
 })
-gauth.LocalWebserverAuth()
+
+gauth.LoadCredentialsFile('credentials_refresh.json')
+if gauth.credentials is None:
+    # gauth.GetFlow()
+    # gauth.flow.params.update({'access_type': 'offline'})
+    # gauth.flow.params.update({'approval_prompt': 'force'})
+
+    gauth.LocalWebserverAuth()
+elif gauth.access_token_expired:
+    gauth.Refresh()
+else:
+    gauth.Authorize()
+gauth.SaveCredentialsFile('credentials_refresh.json')
+
+
 drive = GoogleDrive(gauth)
 
 
@@ -15,6 +33,7 @@ FILEPATH = config('FILEPATH', cast=str)
 
 test_file = drive.CreateFile({'title': FILENAME})
 test_file.SetContentFile(FILEPATH)
+test_file.Upload({'convert': True})
 test_file.InsertPermission({
     'type':  'anyone',
     'value': 'anyone',
@@ -22,4 +41,3 @@ test_file.InsertPermission({
     'additionalRoles': ['commenter'],
     'withLink': True,
 })
-test_file.Upload({'convert': True})
